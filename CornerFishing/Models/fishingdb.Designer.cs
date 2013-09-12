1e1448fb3cac3b6976448dc68d8a94a129e5888f
AQuakenbush <AQuakenbush@gmail.com>
2013-09-11 20:47:54 -0500

    Fixing Jeff's rename nonsense

--- CornerFishing/Models/fishingdb.Designer.cs
+++ CornerFishing/Models/fishingdb.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\JeLee\Desktop\GitHub\CornerFishing\CornerFishing\Models\fishingdb.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Development\CornerFishing\CornerFishing\Models\fishingdb.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
